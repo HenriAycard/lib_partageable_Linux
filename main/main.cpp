294f7f602fc9d8e24baf8018beb45639aa7c4e08
HenriAycard <henriaycard@hotmail.fr>
2021-03-06 11:21:35 +0000

    add correction dlopen and dlsym

--- main/main.cpp
+++ main/main.cpp
@@ -23,29 +23,57 @@ int main(int argc, char ** argv)
 	std::cout << getComposant1Version() << std::endl;
 	std::cout << "valeur 1 :" << valeur1 << " valeur 2 :" << valeur2 << std::endl;
 
-	cout << "Openning main.so..." << endl;
-	void *handle = dlopen("./main.so", RTLD_LAZY);
+	// ===============    DLOPEN() ===============
 
-        if(!handle){
+	cout << "Openning Composant1.so..." << endl;
+	void *handle1 = dlopen("./libComposant1.so", RTLD_LAZY);
+        if(!handle1){
                 cerr<< "Cannot open library: "<< dlerror() << endl;
                 exit(EXIT_FAILURE);
         }
 
-	// load
+	cout << "Openning Composant2.so..." << endl;
+        void *handle2 = dlopen("./libComposant2.so", RTLD_LAZY);
+        if(!handle2){
+                cerr<< "Cannot open library: "<< dlerror() << endl;
+                exit(EXIT_FAILURE);
+        }
+
+
+	// initialisation
+	int (* value1)(int, int);
+	int (* value2)(int, int);
+	char * (* version)();
 	dlerror();
-	int (*functionComposant)(int, int);
-	functionComposant = (int (*)(int, int))dlsym(handle, "composant2");
-        const char *dlsym_error = dlerror();
-	if(dlsym_error){
-		cerr << "Cannot load symbol: " << dlerror()<< endl;
-		dlclose(handle);
-		exit(EXIT_FAILURE);
-	}
 
-	cout << "Valeur du Composant: " << functionComposant(data1, data2) << endl;
+	// COMPOSANT 1
+	*(void **)(&value1) = dlsym(handle1, "_Z10composant1ii");
+	const char *dlsym_error_C1 = dlerror();
+        if(dlsym_error_C1){
+                cerr << "Cannot load symbol composant 1: " << dlerror()<< endl;
+                dlclose(handle1);
+		dlclose(handle2);
+                exit(EXIT_FAILURE);
+        }
 
+	// COMPOSANT 2
+	*(void **)(&value2) = dlsym(handle2, "_Z10composant2ii");
+	const char *dlsym_error_C2 = dlerror();
+        if(dlsym_error_C2){
+                cerr << "Cannot load symbol composant 2: " << dlerror()<< endl;
+                dlclose(handle1);
+		dlclose(handle2);
+                exit(EXIT_FAILURE);
+        }
+
+	// GET VESION
+	*(void **)(&version) = dlsym(handle1, "_Z20getComposant1Versionv");
+
+	cout << (*version)() << endl;
+        cout << "valeur 1 :" << (*value1)(data1, data2) << " valeur 2 :" << (*value2)(data1,data2) << endl;
 	// Close the library
 	cout << "Closing library ..." << endl;
-        dlclose(handle);
+        dlclose(handle1);
+	dlclose(handle2);
 
 }
